af0f81dcdd2d263d97298143d5fb6563b568f226
Jodi Kurniawan <jodikr8s@gmail.com>
2023-09-20 23:22:09 +0700

    Add code for check is instance singleton

--- app/src/main/java/com/dicoding/mysimplelogin/UserRepository.kt
+++ app/src/main/java/com/dicoding/mysimplelogin/UserRepository.kt
@@ -1,10 +1,12 @@
 package com.dicoding.mysimplelogin
 
+import android.util.Log
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UserRepository @Inject constructor(private val sesi: SessionManager) {
+    fun checkInstance() = Log.d("Singleton", "checkInstance: $this")
 
     fun loginUser(username: String) {
         sesi.createLoginSession()
